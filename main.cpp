22e0c20acceff7236051eb007145a1d28e2d72f6
Jack Fenton <mllax8@me.com>
2020-06-05 17:12:51 -0700

    Works when using Hash tables

--- main.cpp
+++ main.cpp
@@ -26,43 +26,93 @@ using namespace std;
 
 //global function findMatches() that is passed the word list and the grid as parameters
 //and which prints out all words that can be found in the grid.
-void findMatches() //pass word list and grid
-{
-	return;
-}
+void findMatches(string& a_wordBank, grid& a_puzzle); //pass word list and grid
+
 
 //global function search(int) which reads the name of the grid file from the keyboard
 //and prints out all words from the word list that can be found in the grid. The function
 //should also print out the CPU time to sort the words, the CPU time to find the words, and
 //the total time. The integer parameter is used to select the sorting algorithm used.
-int search(string a_wordBank, string a_puzzle)
-{
-
-
-}
-
+int search(string& a_wordBank, grid& a_puzzle);
 
 
 
 
 int main()
 {
-
-	grid myPuzzle("puzzle10.txt");
+	
+	grid a_puzzle("puzzle10.txt");
+	
+	//findMatches("wordlist.txt", myPuzzle);
+	
+	string a_wordBank="wordlist2.txt";
+	
+	
+	
+	hashTable<string> myHashTable(a_wordBank, 701);
+	vector<string> allStrings=a_puzzle.getStrings();
+	
+	int numWords=0;
+	
+	for(int check=0;check<allStrings.size();check++)
+	{
+		if(myHashTable.inList(allStrings[check])==1)
+		{
+			cout <<"found: "<<allStrings[check] << endl;
+			numWords++;
+		}
+	}
+	
+	cout<<"Total Words: "<<numWords<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	//cout<<myPuzzle;
 	
 	//cout<<myPuzzle.getChar(-2,0);
 	
-	vector<string> allStrings=myPuzzle.getStrings();
 	
-	for(int it=0; it<allStrings.size();it++)
-		cout<<allStrings[it]<<endl;
+	
+//	for(int it=0; it<allStrings.size();it++)
+//		cout<<allStrings[it]<<endl;
 
 
 	//hashTable<string> myHashTable("wordlist.txt", 701);
 	//cout << myHashTable.inList("livid");
+	
+}
 
 
+void findMatches(string& a_wordBank, grid& a_puzzle) //pass word list and grid
+{
+	int totalMatches=search(a_wordBank, a_puzzle);
+	cout<<"Total Words: "<<totalMatches<<endl;
+}
+
+
+
+int search(string& a_wordBank, grid& a_puzzle)
+{
+	hashTable<string> myHashTable(a_wordBank, 701);
+	vector<string> allStrings=a_puzzle.getStrings();
+	
+	int numWords=0;
+	
+	for(int check=0;check<allStrings.size();check++)
+	{
+		if(myHashTable.inList(allStrings[check]))
+		{
+			cout << allStrings[check] << endl;
+			numWords++;
+		}
+	}
+	
+	return numWords;
 }
 
